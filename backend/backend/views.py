aaf1d3f4e1038fda0f8d02cccc54734c26ba38bf
Cheryl Ma <ma_xxd@163.com>
2021-11-30 00:06:05 +0800

    Complete dashboard pie chart
    Add interactive pie chart to dashboard representing weekly task progress.

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -13,6 +13,10 @@ from bokeh.models import BooleanFilter, CDSView, Select, Range1d, HoverTool
 from bokeh.palettes import Category20
 from bokeh.models.formatters import NumeralTickFormatter
 
+import matplotlib.pyplot as plt
+import base64
+from io import BytesIO
+
 from .utils import *
 from task.models import *
 
@@ -255,15 +259,29 @@ def info_plot_3(selected_symbol):
     return script, div
 
 
+def progress_count():
+    unfinished_tasks = Task.objects.filter(due__lte=datetime.now(), due__gte=datetime.now() +
+                                           timedelta(days=-7), complete_status=False)
+    finished_tasks = Task.objects.filter(due__lte=datetime.now(), due__gte=datetime.now() +
+                                         timedelta(days=-7), complete_status=True)
+    unfinished = len(unfinished_tasks)
+    finished = len(finished_tasks)
+    return unfinished, finished
+
+
 def dashboard(request, task_id=None):
     """if not request.session.get('is_login', None):
         return redirect('/login/')"""
-    
+
+    unfinished, finished = progress_count()
+    print(unfinished, finished)
+
     selected_symbol = 'SPY'
     script, div, last_high, last_close = plot_dashboard(selected_symbol)
 
     keyword = 'stock'
-    news_title_1, news_title_2, news_title_3, news_content_1, news_content_2, news_content_3, news_url_1, news_url_2, news_url_3 = dashboard_news(keyword)
+    news_title_1, news_title_2, news_title_3, news_content_1, news_content_2, news_content_3, news_url_1, news_url_2, news_url_3 = dashboard_news(
+        keyword)
 
     start_time = this_monday()
     month = this_month()
@@ -281,13 +299,50 @@ def dashboard(request, task_id=None):
         form.save()
         return HttpResponseRedirect(reverse('calendar'))
 
+    tasks = Task.objects.filter(due__lte=datetime.now(
+    )+timedelta(days=1), complete_status=False).order_by('due').reverse()
+    if len(tasks) >= 3:
+        lastest_task1 = tasks[0]
+        title1 = lastest_task1.title
+        due1 = lastest_task1.due
+        lastest_task2 = tasks[1]
+        title2 = lastest_task2.title
+        due2 = lastest_task2.due
+        lastest_task3 = tasks[2]
+        title3 = lastest_task3.title
+        due3 = lastest_task3.due
+    elif len(tasks) == 2:
+        lastest_task1 = tasks[0]
+        title1 = lastest_task1.title
+        due1 = lastest_task1.due
+        lastest_task2 = tasks[1]
+        title2 = lastest_task2.title
+        due2 = lastest_task2.due
+        title3 = 'No Third task'
+        due3 = 'No Third task'
+    elif len(tasks) == 1:
+        lastest_task1 = tasks[0]
+        title1 = lastest_task1.title
+        due1 = lastest_task1.due
+        title2 = 'No Second task'
+        due2 = 'No Second task'
+        title3 = 'No Third task'
+        due3 = 'No Third task'
+    elif len(tasks) == 0:
+        title1 = 'No First task'
+        due1 = 'No First task'
+        title2 = 'No Second task'
+        due2 = 'No Second task'
+        title3 = 'No Third task'
+        due3 = 'No Third task'
+
     return render(request, 'Dashboard.html', locals())
 
 
 def info(request):
     """if not request.session.get('is_login', None):
         return redirect('/login/')"""
-    
+
     keyword = 'AAPL AND stock'
 
     selected_symbol = 'AAPL'
