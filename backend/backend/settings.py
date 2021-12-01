a78ecda1c3a203b70e5ae228b6667ee33b200724
Cheryl Ma <ma_xxd@163.com>
2021-12-01 11:41:32 +0800

    Debug Tool for performance by @YeseniaZhuo

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,6 +28,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# change it to your own localhost
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 # Application definition
 
@@ -41,9 +45,11 @@ INSTALLED_APPS = [
     'login',
     'task',
     'finance',
+    "debug_toolbar", # for performance test
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware", # for performance test
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
